2198661854c28863a207ea26259a55b07c633bb6
Piper <skylarpiper710@gmail.com>
2018-12-23 19:00:58 -0600

    fomework push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,33 +5,33 @@ var number = 26;
 
 // // questions... make every question an object in an array
 
-    
+
 // buttons
-$(document).ready(function(){
+$(document).ready(function () {
     $("#quiz").hide();
     $("#finish").hide();
-    
+
 })
 
-$("#startGame").click(function(){
+$("#startGame").click(function () {
     $("#quiz").show();
     $("#instruct").hide();
     run();
-    
+
 })
 
-$("#submit").click(function(){
+$("#submit").click(function () {
     $("#finish").show();
     $("#quiz").hide();
     stop();
     checkAnswer();
     points();
     $("#points").text("Points: " + score);
-   
+
 
 })
 
-$("#reset").click(function(){
+$("#reset").click(function () {
     $("#quiz").hide();
     $("#instruct").show();
     $("#finish").hide();
@@ -39,35 +39,35 @@ $("#reset").click(function(){
     score = 0;
     $("#points").text(score)
     uncheckAnswer();
-   
+
 })
 
-function resetGame(){
+function resetGame() {
     number = 26;
     score = 0;
     $("#countdowntimer").text(number);
 }
 
-   
 
 
 
- 
-    // timer
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
-    }
 
-    
 
-    function decrement() {
+// timer
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
 
-      number--;
 
-      $("#countdowntimer").text(number);
 
-      if (number === 0) {
+function decrement() {
+
+    number--;
+
+    $("#countdowntimer").text(number);
+
+    if (number === 0) {
 
         stop();
 
@@ -76,80 +76,80 @@ function resetGame(){
         points();
 
         $("#finish").show();
-        
+
         $("#quiz").hide()
 
 
 
-      }
     }
+}
 
-    function stop() {
+function stop() {
 
-      clearInterval(intervalId);
-    }
+    clearInterval(intervalId);
+}
 
-    
+
 
 
 
 var nail = document.getElementsByName("nail");
 var car = document.getElementsByName("car");
-var video = document.getElementsByName("video"); 
+var video = document.getElementsByName("video");
 
 var correctNail;
 var correctCar;
 var correctVideo;
 
-function checkAnswer(){
-    for ( var i = 0; i < nail.length; i++){
-        if (nail[i].checked){
-            correctNail=nail[i].value
-        }    
+function checkAnswer() {
+    for (var i = 0; i < nail.length; i++) {
+        if (nail[i].checked) {
+            correctNail = nail[i].value
+        }
 
     };
-    for ( var i = 0; i < car.length; i++){
-        if (car[i].checked){
-            correctCar=car[i].value
-        }    
+    for (var i = 0; i < car.length; i++) {
+        if (car[i].checked) {
+            correctCar = car[i].value
+        }
 
     };
-    for ( var i = 0; i < video.length; i++){
-        if (video[i].checked){
-            correctVideo=video[i].value
-        }    
+    for (var i = 0; i < video.length; i++) {
+        if (video[i].checked) {
+            correctVideo = video[i].value
+        }
 
     }
 }
 
-function points(){
-    if(correctNail === "correct"){
+function points() {
+    if (correctNail === "correct") {
         score++
     }
-    if(correctCar === "correct"){
+    if (correctCar === "correct") {
         score++
     }
-    if(correctVideo === "correct"){
+    if (correctVideo === "correct") {
         score++
     }
 }
 
-function uncheckAnswer(){
-    for ( var i = 0; i < nail.length; i++){
+function uncheckAnswer() {
+    for (var i = 0; i < nail.length; i++) {
         nail[i].checked = false;
-        
-          
+
+
 
     };
-    for ( var i = 0; i < car.length; i++){
+    for (var i = 0; i < car.length; i++) {
         car[i].checked = false;
-            
-          
+
+
 
     };
-    for ( var i = 0; i < video.length; i++){
+    for (var i = 0; i < video.length; i++) {
         video[i].checked = false;
-          
+
 
     }
 }
@@ -165,5 +165,5 @@ function uncheckAnswer(){
 
 
 
-  
-  
+
+
